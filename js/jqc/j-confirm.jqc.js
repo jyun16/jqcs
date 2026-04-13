99a2f32151e5564825f8c21297117c0022e4ed6f
JN <jyun16@gmail.com>
2026-04-14 00:05:15 +0900

    slot...

--- js/jqc/j-confirm.jqc.js
+++ js/jqc/j-confirm.jqc.js
@@ -2,7 +2,7 @@ import jQC from '../../src/jqc.js'
 const d = console.log
 jQC.define('j-confirm', {
   html: `<j-dialog>
-	<p>HOGE</p>
+	<p>{{ msg }}</p>
 	<slot name='footer'>
 		<button class="secondary" @click='close'>Cancel</button>
 		<button class="primary" @click='close' autofocus="autofocus">OK</button>
@@ -12,14 +12,21 @@ jQC.define('j-confirm', {
   css: "",
   globalCss: "",
   p: {
+	msg: ''
 },
   async init() {
 await jQC.import('j-dialog')
 this.render()
 const $dia = jQC.bind('j-dialog', this)
+this.$dia = $dia
   },
   methods: {
-
+open(msg) {
+	this.p.msg = msg
+	this.$dia.render()
+	this.render()
+	this.$dia.open()
+}
   }
 })
 
